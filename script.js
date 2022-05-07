05f95eb3f500ef91c4cfba7dc943ec738d390893
Daria Suhinina <daria.hinina@gmail.com>
2022-05-07 16:57:23 +0400

    done

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-let a = prompt('Сколько будет 15-5?', '');
-if(a == 10){
-    alert('Верно');
-} else {
-    alert('Неверно');
-}
+// Деструктурирующее присваивание
+
+// let user = {
+//     name: "John",
+//     years: 30
+//   };
+
+// let {name, years, isAdmin = false} = user;
+// console.log(name, years, isAdmin);
+
+//Максимальная зарплата
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+
+  function topSalaries() {
+      let max = 0;
+      let maxName = null;
+
+      for(const [name, sal] of Object.entries(salaries)) {
+          if(max < sal) {
+                max = sal;
+                maxName = name;
+          }
+      }
+      return(maxName);
+  }
+
+// function topSalary(salaries) {
+
+//     let max = 0;
+//     let maxName = null;
+  
+//     for(const [name, salary] of Object.entries(salaries)) {
+//       if (max < salary) {
+//         max = salary;
+//         maxName = name;
+//       }
+//     }
+  
+//     return maxName;
+//   }
+
+console.log(topSalaries(salaries));
