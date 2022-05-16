cb3fd72031b8afea10d405fb39833c6333f833c2
Daria Suhinina <daria.hinina@gmail.com>
2022-05-17 00:21:57 +0300

    pn

--- script.js
+++ script.js
@@ -127,3 +127,122 @@
 // let arr = Array.from(str);
 // arr.reverse();
 // document.write(arr);
+
+//   * Описание задачи: Напишите функцию, которая заполняет новый массив предоставленным значением.
+//   * Ожидаемый результат: (3, 'a') => ['a', 'a', 'a']
+//   * Сложность задачи: 1 of 5
+//   * @param {number} arraySize - размер массива
+//   * @param {?} value - значение для массива
+//   * @returns {Array}
+
+// const fill = (arraySize, value) => {
+//     const arr = [];
+//     arr.length = arraySize;
+//     return (arr.fill(value));
+//   }
+  
+//   const data = 3;
+//   const valueToFill = 'a';
+//   alert(fill(data, valueToFill));
+
+// Reverse. Напишите функцию, которая разворачивает массив в обратном порядке. Пожалуйста, не используйте array.reverse(), чтобы сделать задачу более интересной.
+
+// * Описание задачи: Напишите функцию, которая разворачивает массив в обратном порядке.
+// * Ожидаемый результат: [1, 2, 3] => [3, 2, 1]
+// * Сложность задачи: 1 of 5
+// * @param {Array} array - Массив любых элементов
+// * @returns {Array}
+
+//   const reverse = (array) => {
+//     const rev = [];
+//     for(let i = array.length - 1; i >= 0; i--){
+//       rev.push(array[i]);
+//     }
+//     return rev;
+//   }
+//   const data = [1, 2, 3];
+//   document.write(reverse(data)); // [3, 2, 1]
+
+// Compact. Напишите функцию, которая очищает массив от 
+// нежелательных значений, таких как false, undefined, пустые 
+// строки, ноль, null.
+
+
+// const compact = (array) => {
+//     const filter = array.filter(item => !!item)
+//     return filter;
+//   }
+  
+//   const data = [0, 1, false, 2, undefined, '', 3, null];
+//   console.log(compact(data)) // [1, 2, 3[]
+
+const fromPairs = (array) => Object.fromEntries(array);
+
+const data = [['a', 1], ['b', 2]];
+console.log(fromPairs(data)) // { 'a': 1, 'b': 2 }
+
+// Without. Напишите функцию, возвращает новый массив без предоставленных значений. Используйте примитивные типы.
+
+// const without = (array, ...arg) => { 
+//     let [first, second] = arg;
+//     let arr2 = array.filter(item => item != first);
+//     let arr3 = arr2.filter(item => item != second);                       
+//     return (arr3);
+//   }
+  
+//   const data = [1, 2, 3, 1, 2];
+//   console.log(without(data, 1, 2)); // [3]
+
+//Unique. Напишите функцию, которая убирает повторяющиеся значения.
+
+// * Описание задачи: Напишите функцию, которая убирает повторяющиеся значения.
+// * Ожидаемый результат: [1, 2, 3, 1, 2] => [1, 2, 3]
+// * Сложность задачи: 2 of 5
+// * @param {Array<string | number>} array - Массив с примитивными значениями
+// * @returns {Array}
+
+// const unique = (array) => {
+//     let set = new Set(array);
+//     let arr =  Array.from(set);
+//     return (arr)};
+  
+//   const data = [1, 2, 1, 2, 3];
+//   console.log(unique(data)); // [1, 2, 3]
+  
+const array = [1, 2, 1, 2, 3];
+  let newArr = [];
+  for(let i = 0; i <= array.length; i++) {
+    for(let j = 0; j <= i; j++){
+      if(array[i] != newArr[j]){
+        newArr.push(array[i]);
+      }
+    }
+  }
+    console.log (newArr);
+
+// IsEqual. Напишите функцию, которая сравнивает два массива и возвращает true, если они идентичны.
+
+// const isEqual = (firstArray, secondArray) => firstArray.join('') === secondArray.join('');
+
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [1, 2, 3, 4];
+// const arr3 = [1, 2, 3, 5];
+// const arr4 = [1, 2, 3, 4, 5];
+// console.log(isEqual(arr1, arr2)); // true
+// console.log(isEqual(arr1, arr3)); // false
+// console.log(isEqual(arr1, arr4)); // false
+
+const chunk = (array, size) => {
+    const newArr = [];
+    for(let i = 0; i <= array.length; i++){
+      let newArr2 = [].push(array[i]);
+      newArr.push(newArr2);
+    }
+    return (newArr);
+  }
+  
+  const data = [1, 2, 3, 4, 5, 6, 7];
+  console.log(chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7]]
+  console.log(chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7]]
+  
+  
